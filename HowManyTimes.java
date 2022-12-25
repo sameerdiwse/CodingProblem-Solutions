b485b7f5eab0bd13c63dbece85564664e29a5611
sameerdiwse <new@mail.com>
2022-12-25 16:55:15 +0530

    pushing pending

--- HowManyTimes.java
+++ HowManyTimes.java
@@ -1,15 +1,22 @@
 import java.util.Arrays;
+import java.util.stream.*;
+import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.List;
 class HowManyTimes
 {
 	public static void main(String [] args)
 	{
-		int [] a = {2,2,5,1,9,8,5,0,2,4,6,2,7,5,0,5};
+		Integer [] a = {2,2,5,1,9,8,5,0,2,4,6,2,7,5,0,5};
 		Arrays.sort(a);
 		isRepeated(a);
 	}
 	
-	public static void isRepeated(int [] a)
+	public static void isRepeated(Integer[] a)
 	{
-		
+           List<Integer> aa = Arrays.asList(a);
+	       Map<Integer,Long> repeated = aa.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+           System.out.println(repeated);
 	}
 }
