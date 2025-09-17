05f7ad26a7c3db8f3cf441bb1c164671cc344935
sameerdiwse <sameerdiwse9090@gmail.com>
2025-09-17 21:21:51 +0530

    Pushing laptop sorting changes

--- LapSorting.java
+++ LapSorting.java
@@ -8,7 +8,8 @@ class LapSorting
    List<Laptops> l =  Arrays.asList(new Laptops(8000, 8, "black"), new Laptops(7000, 9, "grey"), new Laptops(5000, 7, "yellow"));
    System.out.println("Naturally sorted Laptop list according to Laptop Ram : ");
    List<Laptops> ll = l.stream().sorted(Comparator.comparing(Laptops::getLapRam)).collect(Collectors.toList());
-   
+	//Getting the laptop having maximum ram   
+
    for(Laptops lll : ll)
    {
 	   System.out.println(lll);
