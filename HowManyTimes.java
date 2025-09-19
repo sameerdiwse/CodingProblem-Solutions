f67e0de4f279d560119cdcccf0da24cccfae22cc
sameerdiwse <sameerdiwse9090@gmail.com>
2025-09-20 04:55:36 +0530

    grouping by employees

--- HowManyTimes.java
+++ HowManyTimes.java
@@ -18,5 +18,9 @@ class HowManyTimes
            List<Integer> aa = Arrays.asList(a);
 	       Map<Integer,Long> repeated = aa.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
            System.out.println(repeated);
+//For counting the department wise employees' max salary.
+	Map<String, Optional<Employee>> maxSalary = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.maxBy(Comparator.comparing(Employee::getSalary))));
+			maxSalary.forEach((dept, emp) -> System.out.println(emp.get().getName()+" who works in "+dept+" department gets highest "
+					+ "salary of "+emp.get().getSalary()));
 	}
 }
