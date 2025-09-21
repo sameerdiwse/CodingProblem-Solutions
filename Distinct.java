1ab474fccb96576657f6ddb22e1104e88d01950a
sameerdiwse <sameerdiwse9090@gmail.com>
2025-09-21 22:22:34 +0530

    Added one more logic

--- Distinct.java
+++ Distinct.java
@@ -9,7 +9,11 @@ class Distinct
    {
      //IntStream.of(21,4,7,7,5,8,9,9,14,6,6).sorted(Comparator.reverseOrder()).distinct().forEach(System.out::println);
 	// IntStream.range(1,101).filter(x->x%7==0).forEach(x->System.out.println(x));
-	 long l = IntStream.range(1,101).filter(x->x%7==0).count());
+	 long l = IntStream.range(1,101).filter(x->x%7==0).count();
+
+    // how many evens are there till 100
+    long sums = IntStream.range(0,101).filter(x->x%2==0).count();
+    System.out.println("count of sums: "+sums);
 	 System.out.println(l);
    }
 }
