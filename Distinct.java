e98980a8acc408b14e98323148c74c05e7e948b6
Sameer Diwse <111274016+sameerdiwse@users.noreply.github.com>
2025-10-21 23:21:19 +0530

    Update Distinct.java

--- Distinct.java
+++ Distinct.java
@@ -8,7 +8,7 @@ class Distinct
    public static void main(String [] args)
    {
      //IntStream.of(21,4,7,7,5,8,9,9,14,6,6).sorted(Comparator.reverseOrder()).distinct().forEach(System.out::println);
-	// IntStream.range(1,101).filter(x->x%7==0).forEach(x->System.out.println(x));
+	 IntStream.range(1,101).filter(x->x%7==0).forEach(x->System.out.println(x));
 	 long l = IntStream.range(1,101).filter(x->x%7==0).count();
 
     // how many evens are there till 100
