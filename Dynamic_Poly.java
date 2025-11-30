d7dfa6f8f30d3eb9eb509f8d7ae4b2e3fe9911d4
sameerdiwse <sameerdiwse9090@gmail.com>
2025-11-30 20:08:03 +0530

    pushing polymorphism practice code

---
+++ Dynamic_Poly.java
@@ -0,0 +1,24 @@
+class A
+{
+	void show(){	
+	System.out.println("show A");
+	}
+}
+
+class B extends A
+{
+	void show(){	
+	System.out.println("show B");
+	}
+
+}
+
+class Dynamic_Poly
+{
+	public static void main(String[] args) {
+		A a = new B();
+		a.show();
+
+		
+	}
+}
