727c7d5c8e3ee3c48602b989afc4dbd16723ffb6
sameerdiwse <sameerdiwse9090@gmail.com>
2025-12-01 23:08:17 +0530

    pushing code

---
+++ StringProblem.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class StringProblem
+{
+public static void main(String[] args) {
+	
+		String str = "hello";
+
+		HashMap<Character, Integer> hm = new HashMap<>();
+		for(int i = 0; i<=str.length()-1; i++)
+		{
+			if(hm.containsKey(str.charAt(i)))
+			{
+				hm.put(str.charAt(i), hm.get(str.charAt(i))+1);	
+			}
+			hm.put(str.charAt(i), 1);	
+		}
+
+}
+}
