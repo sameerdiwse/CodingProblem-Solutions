0efb12497274dfda470db54d7f7f5a327c574010
sameerdiwse <sameerdiwse9090@gmail.com>
2025-12-03 22:47:36 +0530

    pushing code

---
+++ StringQuestion.java
@@ -0,0 +1,15 @@
+public class StringQuestion
+{
+	public static void main(String[] args) {
+		//Reverse words in given string
+
+		String string = "I Love Java";
+		String[] str = string.split(" ");
+		String revString = "";	
+		for(int i = str.length; i>0; i--)
+		{
+			revString+=str[i];
+		}
+		System.out.println("this is reversed string: "+revString);
+	}
+}
