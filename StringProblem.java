e2a1e3ee802204deed9131bb0e9b4dd705bf40fa
sameerdiwse <sameerdiwse9090@gmail.com>
2025-12-05 22:59:50 +0530

    solving string problem

--- StringProblem.java
+++ StringProblem.java
@@ -1,24 +1,29 @@
 import java.util.HashMap;
+import java.util.stream.IntStream;
 
 public class StringProblem
 {
 public static void main(String[] args) {
 	
-		String str = "hello";
+		//String str = "hello";
+		//
+		//HashMap<Character, Integer> hm = new HashMap<>();
+		//for(int i = 0; i<=str.length()-1; i++)
+		//{
+		//	if(hm.containsKey(str.charAt(i)))
+		//	{
+		//		hm.put(str.charAt(i), hm.get(str.charAt(i))+1);	
+		//	}
+		//	else
+		//{
+		//	hm.put(str.charAt(i), 1);	
+		//}
+		//}
+		//System.out.println("this is hm: "+hm);
 
-		HashMap<Character, Integer> hm = new HashMap<>();
-		for(int i = 0; i<=str.length()-1; i++)
-		{
-			if(hm.containsKey(str.charAt(i)))
-			{
-				hm.put(str.charAt(i), hm.get(str.charAt(i))+1);	
-			}
-			else
-		{
-			hm.put(str.charAt(i), 1);	
-		}
-		}
-		System.out.println("this is hm: "+hm);
+		String name = "Sameer"; 
+
+		IntStream.range(0, name.length()).filter(i->i%2==0).mapToObj(i->name.charAt(i)).forEach(i->System.out.println(i));
 
 }
 }
