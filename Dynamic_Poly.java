d5ef0b93b1555e3c26236640ac5d1be4d89ee67d
Sameer Diwse <111274016+sameerdiwse@users.noreply.github.com>
2025-12-15 23:16:08 +0530

    Update Dynamic_Poly.java

--- Dynamic_Poly.java
+++ Dynamic_Poly.java
@@ -17,7 +17,7 @@ class Dynamic_Poly
 {
 	public static void main(String[] args) {
 		//B b = new A();//Will give a type mismatch compilation error
-		//Below is the corrected code
+		////Below is the corrected code
 		A a = new B();
 		a.show();//Even if B doesn't have show function, A's show() will be picked up here.
 
