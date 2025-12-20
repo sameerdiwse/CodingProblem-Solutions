ffc16f6f23a997a6092f987ca61dcff149e20a13
sameerdiwse <sameerdiwse9090@gmail.com>
2025-12-20 22:20:36 +0530

    pushing code

--- IntstreamProblem.java
+++ IntstreamProblem.java
@@ -2,5 +2,17 @@ public class IntstreamProblem
 {
 	public static void main(String[] args) {
 	// Find the vowels in the string	
+	String str = "SomeString";
+	Character c;
+	int count = 0;
+		for(int i = 0; i<str.length(); i++)
+		{
+			c = Character.toLowerCase(str.charAt(i));
+			if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+			{
+				count++;		
+			}
+		}
+		System.out.println("this is the count of vowels: "+count);
 	}	
 }
