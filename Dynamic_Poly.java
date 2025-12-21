95e7bc461a3caf127e1aaed4cdad349701836cc4
Sameer Diwse <111274016+sameerdiwse@users.noreply.github.com>
2025-12-21 22:05:32 +0530

    Update Dynamic_Poly.java

--- Dynamic_Poly.java
+++ Dynamic_Poly.java
@@ -16,8 +16,8 @@ class B extends A
 class Dynamic_Poly
 {
 	public static void main(String[] args) {
-		//B b = new A();//Will give a type mismatch compilation error
-		////Below is the corrected code
+		B b = new A();//Will give a type mismatch compilation error
+		//Below is the corrected code
 		A a = new B();
 		a.show();//Even if B doesn't have show function, A's show() will be picked up here.
 System.out.println("checking");
