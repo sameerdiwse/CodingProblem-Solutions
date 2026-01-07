87e7523531021afaeb10120f54a23ff0579fbdea
Sameer Diwse <111274016+sameerdiwse@users.noreply.github.com>
2026-01-07 22:38:23 +0530

    Uncomment distinct number sorting in Distinct.java

--- Distinct.java
+++ Distinct.java
@@ -7,10 +7,10 @@ class Distinct
 {
    public static void main(String [] args)
    {
-     //IntStream.of(21,4,7,7,5,8,9,9,14,6,6).sorted(Comparator.reverseOrder()).distinct().forEach(System.out::println);
+     IntStream.of(21,4,7,7,5,8,9,9,14,6,6).sorted(Comparator.reverseOrder()).distinct().forEach(System.out::println);
 	 IntStream.range(1,101).filter(x->x%7==0).forEach(x->System.out.println(x));
 	 long l = IntStream.range(1,101).filter(x->x%7==0).count();
-
+	System.out.println("this is l: "+l);
     // how many evens are there till 100
     long sums = IntStream.range(0,101).filter(x->x%2==0).count();
     System.out.println("count of sums: "+sums);
