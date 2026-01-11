a3e0bc7c461cf4f2457db2583c0488fc52e8d048
Sameer Diwse <111274016+sameerdiwse@users.noreply.github.com>
2026-01-11 21:57:55 +0530

    Comment out compare method implementation
    Comment out the compare method in IdComparator.

--- IdComparator.java
+++ IdComparator.java
@@ -2,8 +2,8 @@ import java.util.Comparator;
 class IdComparator implements Comparator<Laptops>
 {
    @Override
-   public int compare(Laptops i0, Laptops i1)
-   {
-     return i0.getLapRam()-i1.getLapRam();
-   }
+  // public int compare(Laptops i0, Laptops i1)
+   //{
+   //  return i0.getLapRam()-i1.getLapRam();
+ //  }
 }
