5091b77b726a3e3d9cf563bf896738626aae2fa3
Sameer Diwse <111274016+sameerdiwse@users.noreply.github.com>
2026-01-14 22:21:54 +0530

    Add a new number to the array in RepeatingNum

--- RepeatingNum.java
+++ RepeatingNum.java
@@ -4,7 +4,7 @@ import java.util.stream.Collectors;
 public class RepeatingNum
 {
 	public static void main(String[] args) {
-		int[] arr = {4,5,2,6,3,2,6,4,1,9,9,9,20};
+		int[] arr = {4,5,2,6,3,2,6,4,1,9,9,9,20,10};
 		Arrays.stream(arr).boxed().collect(Collectors.groupingBy(e->e, Collectors.counting())).forEach((num, repeat)->System.out.println(num +" is  repeating "+repeat+" times"));	
 	}
 }
