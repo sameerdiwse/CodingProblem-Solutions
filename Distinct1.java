eef420ae3d4a5e1add88bcb58b75ef286df4bed6
Sameer Diwse <111274016+sameerdiwse@users.noreply.github.com>
2026-02-06 22:16:36 +0530

    Refactor Distinct1 to find max value more efficiently

--- Distinct1.java
+++ Distinct1.java
@@ -1,20 +1,20 @@
-import java.util.*;
-public class Distinct1
-{
-  public static void main(String [] args)
-  {
-	 int num = 123;
-	// System.out.println(num[0]);
+import java.util.Arrays;
 
-int[] i = {1,4,23,103,54,3,21, 521,90};
-//			List<int[]> something = Arrays.asList(i);
-//			System.out.println(something.get(0)[4]);
-			//Arrays.stream(i).boxed().sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue).toArray();
-			int maxValue = Arrays.stream(i).boxed().max(Integer::compare).get();
-			System.out.println("maxValue: "+maxValue);
-			
-			int[] a = Arrays.stream(i).boxed().sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue).toArray();
-			System.out.println(a[0]);
-			a[0]; -> is not valid; it's just a standalone expression and does nothing.
-  }
+public class Distinct1 {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 4, 23, 103, 54, 3, 21, 521, 90};
+
+        // Max value (most efficient)
+        int max = Arrays.stream(arr).max().orElseThrow();
+        System.out.println("Max Value: " + max);
+
+        // Highest element after sorting (if you really need sorted array)
+        int[] sortedDesc = Arrays.stream(arr)
+                                 .sorted()
+                                 .toArray();
+
+        int highest = sortedDesc[sortedDesc.length - 1];
+        System.out.println("Highest (via sort): " + highest);
+    }
 }
