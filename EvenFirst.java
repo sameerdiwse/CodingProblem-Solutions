b32d06a14789fed1ae9eec9bd19fccb867a27c63
Sameer Diwse <111274016+sameerdiwse@users.noreply.github.com>
2026-02-13 21:14:24 +0530

    Refactor EvenFirst class to rearrange evens first

--- EvenFirst.java
+++ EvenFirst.java
@@ -1,32 +1,36 @@
-class EvenFirst
-{
+import java.util.Arrays;
 
-  public static void main(String [] args)
-  {
-    int [] arr = {1,2,3,4,5,6,7,8,9,10};
-	firstEvenAndOdd(arr);
-  }
-  
-  public static void firstEvenAndOdd(int arr1 [])
-  {
-	  int [] a = new int [arr1.length];
-	  int count = 0;
-	for(int i = 0; i<arr1.length; i++)
-	{
-		if(arr1[i] % 2 == 0)
-		{
-			a[count] = arr1[i];
-			count++;
-		}
-	}
-	for(int i = 0; i<arr1.length; i++)
-	{
-		if(arr1[i] % 2 != 0)
-		{
-		  a[count] = arr1[i];
-		  count++;
-		}
-	}
-	System.out.print(a);
-  }
+class EvenFirst {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int[] result = rearrangeEvenFirst(arr);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] rearrangeEvenFirst(int[] input) {
+
+        if (input == null || input.length == 0) {
+            return new int[0];
+        }
+
+        int[] result = new int[input.length];
+        int index = 0;
+
+        // Add even numbers first
+        for (int num : input) {
+            if (num % 2 == 0) {
+                result[index++] = num;
+            }
+        }
+
+        // Add odd numbers next
+        for (int num : input) {
+            if (num % 2 != 0) {
+                result[index++] = num;
+            }
+        }
+
+        return result;
+    }
 }
